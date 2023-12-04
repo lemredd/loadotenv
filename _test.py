6cd5894ca10ddafc0db54e16c9ad586360e4cdfe
Lem Redd <lmoa.jhdp@gmail.com>
2023-12-05 07:25:17 +0800

    test(main): use `unittest` test case

--- _test.py
+++ _test.py
@@ -1,2 +1,11 @@
-foo = 123
-print(foo)
+import unittest
+
+
+class TestCoreFunctionality(unittest.TestCase):
+
+    def test_loading_of_environment_vars(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
