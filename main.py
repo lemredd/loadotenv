86e3b385376e5ed8ebec192769c7235233a224f9
Lem Redd <lmoa.jhdp@gmail.com>
2023-12-04 18:52:35 +0800

    feat: make function to load `.env`

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+
+def load_env(file_loc=".env"):
+    with open(file_loc) as f:
+        env_vars = [
+            var.split("=") for var in f.readlines()
+            if "=" in var and "#" not in var
+        ]
+
+        for k, v in env_vars:
+            os.environ[k] = v
