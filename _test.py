d6769cb69217e5427ddebdf3f98c5927693f215f
Lem Redd <lmoa.jhdp@gmail.com>
2023-12-05 07:51:59 +0800

    test(main): set up and tear down

--- _test.py
+++ _test.py
@@ -1,10 +1,24 @@
+import os
 import unittest
 
 
 class TestCoreFunctionality(unittest.TestCase):
+    def setUp(self):
+        self.FILE_LOC = "./.env_fixture"
+
+        lines = """
+# This is a comment
+foo=bar
+        """
+
+        with open(self.FILE_LOC, mode="w") as test_file:
+            test_file.write(lines)
 
     def test_loading_of_environment_vars(self):
         self.assertEqual(1, 1)
+
+    def tearDown(self):
+        os.remove(self.FILE_LOC)
 
 
 if __name__ == '__main__':
