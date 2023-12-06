5c71a574809ce71464d4bbeddb743c16dead6f5d
Lem Redd <lmoa.jhdp@gmail.com>
2023-12-06 18:57:32 +0800

    test(main): prepare testing for edg cases

--- _test.py
+++ _test.py
@@ -24,5 +24,24 @@ foo=bar
         os.remove(self.FILE_LOC)
 
 
+class EdgeCasesTestCase():
+    def setUp(self):
+        self.FILE_LOC = "./.env_fixture"
+
+        lines = """
+# This is a comment
+foo=bar
+        """
+
+        with open(self.FILE_LOC, mode="w") as test_file:
+            test_file.write(lines)
+
+    def test_erroneous_lines(self):
+        self.assertEqual(1, 1)
+
+    def tearDown(self):
+        os.remove(self.FILE_LOC)
+
+
 if __name__ == '__main__':
     unittest.main()
