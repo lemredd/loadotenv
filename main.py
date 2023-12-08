d2d9ce19b06391ce1f664606f5d4925c9fe167fe
Lem Redd <lmoa.jhdp@gmail.com>
2023-12-08 11:49:56 +0800

    fix(main): handle edge case of multiple `=`

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def load_env(file_loc=".env"):
     with open(file_loc) as f:
         env_vars = [
             var.strip("\n").split("=") for var in f.readlines()
-            if "=" in var and "#" not in var
+            if "=" in var  # is a valid variable
+            and "#" not in var  # Is not a comment
+            and var.count("=") == 1  # Is a valid variable (only 1 `=`)
         ]
 
         for k, v in env_vars:
